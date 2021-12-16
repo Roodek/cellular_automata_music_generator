0650d0170e881e37a1f6851f03f0f0732cc34974
Roodek <radmatyja@gmail.com>
2021-12-16 20:18:33 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import itertools
+from IPython.display import HTML
+from music21 import *
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
