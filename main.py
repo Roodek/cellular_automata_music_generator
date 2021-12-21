ba7315befde0841072a4837c52fc9fed1bb091d2
Roodek <radmatyja@gmail.com>
2021-12-22 00:50:51 +0100

    added setting key signatures

--- main.py
+++ main.py
@@ -78,33 +78,79 @@ class Chords_Generator():
 
 def get_major_pentatonic_scale(scale_name):
     scale_arr = scale_name.value
-    pentatonic = [scale_arr[0] + str(DEFAULT_OCTAVE), scale_arr[1] + str(DEFAULT_OCTAVE),
-                  scale_arr[2] + str(DEFAULT_OCTAVE), scale_arr[4] + str(DEFAULT_OCTAVE),
-                  scale_arr[5] + str(DEFAULT_OCTAVE),scale_arr[0] + str(DEFAULT_OCTAVE + 1)]
+    pentatonic = [scale_arr[0], scale_arr[1],
+                  scale_arr[2], scale_arr[4],
+                  scale_arr[5], scale_arr[0]]
     return pentatonic
 
 
 def get_zelda_scale():
-    return [SCALES.C_MAJOR.value[1] + str(DEFAULT_OCTAVE), SCALES.C_MAJOR.value[3] + str(DEFAULT_OCTAVE),
-            SCALES.C_MAJOR.value[5] + str(DEFAULT_OCTAVE), SCALES.C_MAJOR.value[7] + str(DEFAULT_OCTAVE),
-            SCALES.C_MAJOR.value[1] + str(DEFAULT_OCTAVE + 1)]
+    return [SCALES.C_MAJOR.value[1], SCALES.C_MAJOR.value[3],
+            SCALES.C_MAJOR.value[5], SCALES.C_MAJOR.value[7],
+            SCALES.C_MAJOR.value[1]]
 
 
 def get_minor_pentatonic_scale(scale_name):
     scale_arr = scale_name.value
-    pentatonic = [scale_arr[0] + str(DEFAULT_OCTAVE), scale_arr[2] + str(DEFAULT_OCTAVE),
-                  scale_arr[3] + str(DEFAULT_OCTAVE), scale_arr[4] + str(DEFAULT_OCTAVE),
-                  scale_arr[6] + str(DEFAULT_OCTAVE), scale_arr[0] + str(DEFAULT_OCTAVE + 1)]
+    pentatonic = [scale_arr[0], scale_arr[2],
+                  scale_arr[3], scale_arr[4],
+                  scale_arr[6], scale_arr[0]]
     return pentatonic
 
 
 def binary_to_dec(binary):
     return int(binary, 2)
 
+def lower_by_halfstep(note):
+    if len(note) ==1 or note.endswith('-'):
+        return note+'-'
+    elif note.endswith('#'):
+        return note[0]
+
+def raise_by_halfstep(note):
+    if len(note) == 1 or note.endswith('#'):
+        return note + '#'
+    elif note.endswith('-'):
+        return note[0]
+
+
+
 class MusicGenerator():
     def __init__(self):
         self.stream = stream.Stream()
-        super()
+        self.current_octave = DEFAULT_OCTAVE
+        self.stream.keySignature = key.KeySignature(0)
+
+    def set_key_signature(self,scale):
+        if scale == SCALES.G_MAJOR or scale == SCALES.E_MINOR:
+            self.stream.keySignature = key.KeySignature(1)
+        elif scale == SCALES.D_MAJOR or scale == SCALES.B_MINOR:
+            self.stream.keySignature = key.KeySignature(2)
+        elif scale == SCALES.A_MAJOR or scale == SCALES.FIS_MINOR:
+            self.stream.keySignature = key.KeySignature(3)
+        elif scale == SCALES.E_MAJOR or scale == SCALES.CIS_MINOR:
+            self.stream.keySignature = key.KeySignature(4)
+        elif scale == SCALES.B_MAJOR or scale == SCALES.GIS_MINOR:
+            self.stream.keySignature = key.KeySignature(5)
+        elif scale == SCALES.FIS_MAJOR or scale == SCALES.DIS_MINOR:
+            self.stream.keySignature = key.KeySignature(6)
+        elif scale == SCALES.F_MAJOR or scale == SCALES.D_MINOR:
+            self.stream.keySignature = key.KeySignature(-1)
+        elif scale == SCALES.BFLAT_MAJOR or scale == SCALES.G_MINOR:
+            self.stream.keySignature = key.KeySignature(-2)
+        elif scale == SCALES.EFLAT_MAJOR or scale == SCALES.C_MINOR:
+            self.stream.keySignature = key.KeySignature(-3)
+        elif scale == SCALES.AFLAT_MAJOR or scale == SCALES.F_MINOR:
+            self.stream.keySignature = key.KeySignature(-4)
+        elif scale == SCALES.DFLAT_MAJOR or scale == SCALES.BFLAT_MINOR:
+            self.stream.keySignature = key.KeySignature(-5)
+        elif scale == SCALES.GFLAT_MAJOR or scale == SCALES.EFLAT_MINOR:
+            self.stream.keySignature = key.KeySignature(-6)
+        else:
+            return
+
+    def set_time_signature(self):
+        self.stream.timeSignature = meter.TimeSignature('4/4')
 
     def figure_out_note(self,note_name_val,octave):
         written_octave = octave
@@ -113,7 +159,7 @@ class MusicGenerator():
         name = SCALES.C_MAJOR.value[note_name_val] + str(written_octave)
         return name
 
-    def figure_out_duration(self,note_length_code):
+    def figure_out_duration(self, note_length_code):
         return DURATIONS[note_length_code]
 
     def create_note(self,note_name_val, note_length=LENGTHS.QUARTER.value, octave=DEFAULT_OCTAVE):
@@ -173,31 +219,43 @@ def generate_random_matrix():
         x.append(y)
     return x
 
+def music_demo():
+    note.Note('G##').show()
+
 if __name__ == '__main__':
-    matrix = [[1, 1, 0, 0, 1, 1, 0, 0, 1],
-              [1, 0, 1, 0, 0, 1, 1, 0, 1],
-              [0, 1, 1, 0, 1, 1, 0, 0, 1],
-              [0, 0, 0, 1, 1, 0, 0, 1, 1],
-              [1, 1, 0, 0, 1, 0, 0, 0, 1],
-              [1, 0, 0, 0, 1, 0, 0, 0, 1],
-              [1, 1, 0, 0, 1, 1, 0, 0, 1],
-              [1, 1, 0, 0, 1, 1, 0, 0, 1],
-              [1, 1, 0, 0, 1, 1, 0, 0, 1]]
+    # matrix = [[1, 1, 0, 0, 1, 1, 0, 0, 1],
+    #           [1, 0, 1, 0, 0, 1, 1, 0, 1],
+    #           [0, 1, 1, 0, 1, 1, 0, 0, 1],
+    #           [0, 0, 0, 1, 1, 0, 0, 1, 1],
+    #           [1, 1, 0, 0, 1, 0, 0, 0, 1],
+    #           [1, 0, 0, 0, 1, 0, 0, 0, 1],
+    #           [1, 1, 0, 0, 1, 1, 0, 0, 1],
+    #           [1, 1, 0, 0, 1, 1, 0, 0, 1],
+    #           [1, 1, 0, 0, 1, 1, 0, 0, 1]]
+    #
+    # matrix2 = [[1, 1, 0, 0, 1, 1, 0, 0, 1],
+    #           [1, 0, 1, 0, 0, 1, 1, 0, 1],
+    #           [0, 1, 1, 0, 1, 1, 0, 0, 1],
+    #           [0, 0, 0, 1, 1, 0, 0, 1, 1],
+    #           [1, 1, 0, 0, 1, 0, 0, 0, 1],
+    #           [1, 0, 0, 0, 1, 0, 0, 0, 1],
+    #           [1, 1, 0, 0, 1, 1, 0, 0, 1],
+    #           [1, 1, 0, 0, 1, 1, 0, 0, 1],
+    #           [1, 1, 0, 0, 1, 1, 0, 0, 1]]
+    #
+    # matrices=[generate_random_matrix(),generate_random_matrix(),generate_random_matrix()]
+    # music_generator = MusicGenerator()
+    # for m in matrices:
+    #     parse_matrix(music_generator,m)
+    # music_generator.finalize()
+    print(raise_by_halfstep('G'))
+    print(raise_by_halfstep('G#'))
+    print(raise_by_halfstep('G-'))
 
-    matrix2 = [[1, 1, 0, 0, 1, 1, 0, 0, 1],
-              [1, 0, 1, 0, 0, 1, 1, 0, 1],
-              [0, 1, 1, 0, 1, 1, 0, 0, 1],
-              [0, 0, 0, 1, 1, 0, 0, 1, 1],
-              [1, 1, 0, 0, 1, 0, 0, 0, 1],
-              [1, 0, 0, 0, 1, 0, 0, 0, 1],
-              [1, 1, 0, 0, 1, 1, 0, 0, 1],
-              [1, 1, 0, 0, 1, 1, 0, 0, 1],
-              [1, 1, 0, 0, 1, 1, 0, 0, 1]]
+    print(lower_by_halfstep('G'))
+    print(lower_by_halfstep('G#'))
+    print(lower_by_halfstep('G-'))
 
-    matrices=[generate_random_matrix(),generate_random_matrix(),generate_random_matrix()]
-    music_generator = MusicGenerator()
-    for m in matrices:
-        parse_matrix(music_generator,m)
-    music_generator.finalize()
+
 
 
